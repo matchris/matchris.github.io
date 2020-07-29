1dd451a08ca3e164eedbb079d5e1e7c0e9a80bb2
matchris <krzysztof.matyjaszek@outlook.com>
2020-07-29 23:08:31 +0200

    Initial commit 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://dashboard.heroku.com/apps/stark-shelf-61704/v1/task/';
+  var apiRoot = 'https://safe-earth-18840.herokuapp.com/v1/task/';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
